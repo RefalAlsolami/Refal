879a0a73892689c29bbc731873107a5f3b14356d
h2020 <h2020@Azo0oz>
2024-09-01 17:12:25 +0300

    my first commit

---
+++ OneDrive/المستندات/NetBeansProjects/SimpleProject/src/main/java/com/mycompany/simpleproject/SimpleProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.simpleproject;
+
+/**
+ *
+ * @author h2020
+ */
+public class SimpleProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
